98e682ccbec8aa6ef7d24e2a5bd784d4c3ca2da1
augustofklein <augusto_klein@hotmail.com>
2023-12-06 23:28:17 -0300

    Adicao de exemplo JSX

---
+++ exercicios/pages/basicos/jsx1.js
@@ -0,0 +1,6 @@
+// JSX
+// Função de transpiler realiza a conersão do código em java script, para que
+// o browser possa interpretar o código HTML.
+export default function jsx1Diferente(){
+    return <h1>Segundo</h1>
+}
