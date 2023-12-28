beee99981160536b2ce4657015af92f7aa282c58
augustofklein <augusto_klein@hotmail.com>
2023-12-27 21:35:46 -0300

    Exemplo de repeticao

---
+++ exercicios/data/listaProdutos.js
@@ -0,0 +1,8 @@
+import Produto from "../model/produto";
+
+export default [
+    new Produto(1, "iPad", 7543),
+    new Produto(2, "Geladeira", 1234),
+    new Produto(3, "Bancada", 888),
+    new Produto(4, "Sofa", 987)
+]
