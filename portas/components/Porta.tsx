d5c19bb3157b868b6b7526cde9ac9ba0c8e9f39c
augustofklein <augusto_klein@hotmail.com>
2024-01-11 07:12:10 -0300

    Adicao de procedimentos para controle da abertura das portas

---
+++ portas/components/Porta.tsx
@@ -0,0 +1,42 @@
+import PortaModel from '../model/porta'
+import styles from '../src/styles/Porta.module.css'
+
+interface PortaProps {
+    value: PortaModel
+    onChange: (novaPorta: PortaModel) => void
+}
+
+export default function Porta(props: PortaProps) {
+
+    const porta = props.value
+    const selecionada = porta.selecionada && !porta.aberta ? styles.selecionada : ''
+
+    function alternarSelecao() {
+        props.onChange(porta.alterarSelecao())
+    }
+
+    const abrirPorta = (e: React.MouseEvent) => {
+        props.onChange(porta.abrir())
+        e.stopPropagation()
+    }
+
+    function renderizarPorta() {
+        return(
+            <div className={styles.porta}>
+                <div className={styles.numero}>{porta.numero}</div>
+                <div className={styles.macaneta} onClick={abrirPorta}></div>
+            </div>
+            
+        )
+    }
+
+    return(
+        <div className={styles.area} onClick={alternarSelecao}>
+            <div className={`${styles.estrutura} ${selecionada}`}>
+                {porta.aberta ? false: renderizarPorta()}
+            </div>
+            <div className={styles.chao}></div>
+        </div>
+    )
+
+}
---
+++ portas/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Presente from '../../components/Presente'
+import Porta from '../../components/Porta'
+import { useState } from 'react'
+import PortaModel from '../../model/porta'
+
+export default function Home() {
+  
+  const [p1, setP1] = useState(new PortaModel(1))
+  
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)}/>
+    </div>
+  )
+}
