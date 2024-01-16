b7d043b9a4f1568ffabd686aa671fdbde8c82b16
augustofklein <augusto_klein@hotmail.com>
2024-01-15 22:07:15 -0300

    Adicao de ajustes nos botoes de informe das propriedades

--- portas/src/pages/index.tsx
+++ portas/src/pages/index.tsx
@@ -1,22 +1,37 @@
 import styles from "../styles/Formulario.module.css"
 import Cartao from "../../components/Cartao"
 import Link from "next/link"
+import EntradaNumerica from "../../components/EntradaNumerica"
+import { useState } from "react"
 
 export default function Formulario() {
   
+  const [qtdePortas, setQtdePortas] = useState(3)
+  const [comPresente, setComPresente] = useState(1)
+
   return (
     <div className={styles.formulario}>
       <div>
         <Cartao bgcolor="#c0392c">
           <h1>Monty Hall</h1>
         </Cartao>
-        <Cartao></Cartao>
+        <Cartao>
+          <EntradaNumerica text="Qtde Portas?"
+            value={qtdePortas}
+            onChange={novaQtde => setQtdePortas(novaQtde)}>
+          </EntradaNumerica>
+        </Cartao>
       </div>
       <div>
-        <Cartao></Cartao>
+        <Cartao>
+          <EntradaNumerica text="Porta com Presente?"
+            value={comPresente}
+            onChange={novaPortaComPresente => setComPresente(novaPortaComPresente)}>
+          </EntradaNumerica>
+        </Cartao>
 
         <Cartao bgcolor="#28a085">
-          <Link href={`/jogo/4/2`}>
+          <Link href={`/jogo/${qtdePortas}/${comPresente}`}>
             <h2 className={styles.link}>Iniciar</h2>
           </Link>
         </Cartao>
