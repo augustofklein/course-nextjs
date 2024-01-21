d2623024eef30a99811b02f569095cfa8f125602
augustofklein <augusto_klein@hotmail.com>
2024-01-21 20:10:15 -0300

    Adicao do uso de context API para realizar a alterancia do tema da aplicacao

--- admin-template/src/pages/notificacoes.tsx
+++ admin-template/src/pages/notificacoes.tsx
@@ -1,9 +1,13 @@
 import Layout from "@/components/template/Layout";
+import useAppData from "@/data/hook/useAppData";
 
 export default function Notificacoes() {
-  return (
-    <Layout titulo="Notificações" subtitulo="Aqui você irá gerenciar as suas notificações!">
-      <h3>Conteúdo</h3>
-    </Layout>
-  )
+  
+    const { alternarTema } = useAppData()
+  
+    return (
+        <Layout titulo="Notificações" subtitulo="Aqui você irá gerenciar as suas notificações!">
+            <button onClick={alternarTema}>Clique para alternar o tema</button>
+        </Layout>
+    )
 }
