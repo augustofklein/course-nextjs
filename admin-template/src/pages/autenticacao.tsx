af5766bea2a5074b95aeb95fe9a41504ecf94f95
augustofklein <augusto_klein@hotmail.com>
2024-01-23 07:08:31 -0300

    Adicao de novos ajustes no layout

--- admin-template/src/pages/autenticacao.tsx
+++ admin-template/src/pages/autenticacao.tsx
@@ -16,11 +16,15 @@ export default function Autenticacao() {
     }
 
     return(
-        <div className="flex flex-col h-screen items-center justify-center">
-            <div className="w-1/2">
-                <h1 className={`
-                    text-xl font-bold mb-5
-                `}>
+        <div className="flex h-screen items-center justify-center">
+            <div className="hidden md:block md:w-1/2 lg:w-2/3">
+                <img
+                    src="https://source.unsplash.com/random"
+                    alt="Imagem da Tela de Autenticação"
+                    className="h-screen w-full object-cover"/>
+            </div>
+            <div className="m-10 w-full md:w-1/2 lg:w-1/3">
+                <h1 className={`text-3xl font-bold mb-5`}>
                     {modo === 'login' ? 'Entre com a Sua Conta' : 'Cadastre-se na Plataforma'}
                 </h1>
                 <AuthInput
@@ -53,6 +57,24 @@ export default function Autenticacao() {
                 `}>
                     Entrar com o Google
                 </button>
+
+                {modo === 'login' ? (
+                    <p className="mt-8">
+                        Novo por aqui?
+                        <a onClick={() => setModo('cadastro')} className={`
+                            text-blue-500 hover:text-blue-700 font-semibold
+                            cursor-pointer
+                        `}> Crie uma Conta Gratuitamente</a>
+                    </p>
+                ) : (
+                    <p className="mt-8">
+                        Já faz parte da nossa Comunidade
+                        <a onClick={() => setModo('login')} className={`
+                            text-blue-500 hover:text-blue-700 font-semibold
+                            cursor-pointer
+                        `}> Entre com as suas credenciais</a>
+                    </p>
+                )}
             </div>
         </div>
     )
