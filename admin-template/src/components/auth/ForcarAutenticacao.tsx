2f2ae1aee608178c8fc5d4c0036e61630b9de5ce
augustofklein <augusto_klein@hotmail.com>
2024-02-01 07:07:29 -0300

    Adicao de script para protecao das rotas

--- admin-template/src/components/auth/ForcarAutenticacao.tsx
+++ admin-template/src/components/auth/ForcarAutenticacao.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import loading from '../../../public/images/loading.gif'
 import useAuth from '@/data/hook/useAuth'
 import router from 'next/router'
+import Head from 'next/head'
 
 interface ForcarAutenticacaoProps {
     children?:any
@@ -18,6 +19,17 @@ export default function ForcarAutenticacao(props: ForcarAutenticacaoProps) {
     function renderizarConteudo() {
         return(
             <>
+                <Head>
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                                if(!document.cookie?.includes("admin-template-auth")) {
+                                    window.location.href = "/autenticacao"
+                                }  
+                            `
+                        }}
+                    />
+                </Head>
                 {props.children}
             </>
         )
