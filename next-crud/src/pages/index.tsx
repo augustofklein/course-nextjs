6c95d6285fcb4bf33a2f9be21f084e586c6dffbd
augustofklein <augusto_klein@hotmail.com>
2024-02-08 07:09:20 -0300

    Adicao de novos componentes do layout da tabela

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -1,9 +1,27 @@
 import Layout from '@/components/Layout'
 import { Inter } from 'next/font/google'
+import Cliente from '../core/Cliente'
+import Tabela from '@/components/Tabela'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 29, '2'),
+    new Cliente('Carlos', 22, '3'),
+    new Cliente('João', 78, '4')
+  ]
+
+  function clienteSelecionado(cliente: Cliente) {
+    console.log(cliente.nome)
+  }
+
+  function clienteExcluido(cliente: Cliente) {
+    console.log(`Exluir ${cliente.nome}`)
+  }
+
   return (
     <div className={`
       flex justify-center items-center h-screen
@@ -11,7 +29,11 @@ export default function Home() {
       text-white
     `}>
       <Layout titulo='Cadastro Simples'>
-        <span>Conteudo</span>
+        <Tabela clientes={clientes}
+          clienteSelecionado={clienteSelecionado}
+          clienteExcluido={clienteExcluido}
+        >  
+          </Tabela>
       </Layout>
     </div>
   )
