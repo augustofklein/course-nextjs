36a2c39fe5164876015a6aa2da0875e76ecfb2f3
augustofklein <augusto_klein@hotmail.com>
2024-02-15 21:59:07 -0300

    Adicao de tratamento para carregamento dos dados da questao

--- quiz/src/pages/api/questoes/[id].ts
+++ quiz/src/pages/api/questoes/[id].ts
@@ -6,10 +6,16 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-
     if (typeof req.query.id !== 'undefined') {
         const id = +req.query.id
 
-        res.status(200).json(questoes[0].paraObjeto())
+        const questao = questoes.filter(questao => questao.id === id)
+
+        if(questao.length === 1) {
+            const questaoSelecionada = questao[0]
+            res.status(200).json(questaoSelecionada.paraObjeto())
+        } else {
+            res.status(204).send('')
+        }
     }
 }
