15f00f68da00d7e47b155b7190eeac0d670774fb
augustofklein <augusto_klein@hotmail.com>
2024-02-17 19:13:19 -0300

    Adicao da chamada para retorno dos dados das questoes

--- quiz/src/pages/index.tsx
+++ quiz/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Questionario from "@/components/Questionario";
 import QuestaoModel from "@/model/questao";
 import RespostaModel from "@/model/resposta";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const questaoMock = new QuestaoModel(1, 'Melhor cor?', [
   RespostaModel.errada('Verde'),
@@ -10,10 +10,34 @@ const questaoMock = new QuestaoModel(1, 'Melhor cor?', [
   RespostaModel.certa('Preta')
 ])
 
+const BASE_URL = 'http://localhost:3000/api'
+
 export default function Home() {
 
+  const [idsDasQuestoes, setIdsDasQuestoes] = useState<number[]>([])
   const [questao, setQuestao] = useState(questaoMock)
   
+  async function carregarIdsDasQuestoes() {
+    const resp = await fetch(`${BASE_URL}/questionario`)
+    const idsDasQuestoes = await resp.json()
+    console.log(idsDasQuestoes)
+    setIdsDasQuestoes(idsDasQuestoes)
+  }
+
+  async function carregarQuestao(idQuestao: number) {
+    const resp = await fetch(`${BASE_URL}/questoes/${idQuestao}`)
+    const json = await resp.json()
+    console.log(json)
+  }
+
+  useEffect(() => {
+    carregarIdsDasQuestoes()
+  }, [])
+
+  useEffect(() => {
+    idsDasQuestoes.length > 0 && carregarQuestao(idsDasQuestoes[0])
+  }, [idsDasQuestoes])
+
   function questaoRespondida() {
 
   }
@@ -23,19 +47,11 @@ export default function Home() {
   }
 
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh'
-    }}>
-      <Questionario
-        questao={questao}
-        ultima={true}
-        questaoRespondida={questaoRespondida}
-        irPraProximoPasso={irPraProximoPasso}
-      />
-    </div>
+    <Questionario
+      questao={questao}
+      ultima={true}
+      questaoRespondida={questaoRespondida}
+      irPraProximoPasso={irPraProximoPasso}
+    />
   )
 }
